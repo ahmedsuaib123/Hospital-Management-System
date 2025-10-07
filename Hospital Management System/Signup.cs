b298ebe55ef67fa30dd4727d3ca3061af3f2d30c
DeLL <DeLL@DESKTOP-HOP36BN>
2025-10-07 17:04:44 +0600

    Updated

--- Hospital Management System/Signup.cs
+++ Hospital Management System/Signup.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Hospital_Management_System
 {
-    public partial class Form1 : Form
+    public partial class Signup : Form
     {
-        public Form1()
+        public Signup()
         {
             InitializeComponent();
         }
